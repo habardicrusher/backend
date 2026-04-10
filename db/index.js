f253c83a98942f57da46b9b1ddb8ada9f061a2b5
habardicrusher <habardicrushher@gmail.com>
2026-04-10 19:28:34 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,17 @@
+// db/index.js
+const { Pool } = require('pg');
 
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://neondb_owner:npg_HGwqC4TJaXD6@ep-dawn-king-a11873v3-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+
+const pool = new Pool({
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
+
+pool.connect((err, client, release) => {
+    if (err) return console.error('❌ خطأ في الاتصال بقاعدة البيانات:', err.stack);
+    console.log('✅ تم الاتصال بقاعدة البيانات بنجاح');
+    release();
+});
+
+module.exports = { pool };
